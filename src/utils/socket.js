4163c3cb37be320157c734cdf7c709bc3c93e64c
manveerKaurB <manveerkaur1664@gmail.com>
2025-04-01 10:45:13 +0530

    socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,11 +1,10 @@
-import {io} from "socket.io-client";
-import {BASE_URL} from "./Constants";
+import io from "socket.io-client";
+import { BASE_URL } from "./constants";
+
 export const createSocketConnection = () => {
-    // base_url is backend url here, connecting with server
-    if(location.host === 'localhost'){
-        return io(BASE_URL);
-    }
-    else {
-        return io("/", {path: "/api/socket.io"});
-    }
-}
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
+};
